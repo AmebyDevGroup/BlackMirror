a6afcc128176aee861f57f34dc76479131b74642
Krzysztof Wieczorek <krzysztof.wieczorek@sungroup.pl>
2019-10-27 00:23:22 +0200

    Poprawa websocket po stronie Vue

--- smartmirror_vue/src/main.js
+++ smartmirror_vue/src/main.js
@@ -1,5 +1,17 @@
 import Vue from 'vue'
 import App from './App.vue'
+import Echo from 'laravel-echo';
+
+window.Pusher = require('pusher-js');
+
+window.Echo = new Echo({
+    broadcaster: 'pusher',
+    key: 123456,
+    cluster: 'mt1',
+    // encrypted: true
+    wsHost: 'localhost',
+    wsPort: 6001
+});
 
 Vue.config.productionTip = false
 
