ddac47973b28c20779aa0d5fbf4d68a8beb64928
Jakub Lorc <lorc.jakub@gmail.com>
2019-11-24 09:18:30 +0100

    env fix

---
+++ smartmirror_vue/src/components/Weather.vue
@@ -0,0 +1,21 @@
+<template>
+  {{test}}
+</template>
+
+<script>
+export default {
+  name: 'Weather',
+  data() {
+    return {
+      title: 'Test',
+    }
+  },
+  mounted() {
+
+  }
+}
+</script>
+
+<style lang="less">
+
+</style>
